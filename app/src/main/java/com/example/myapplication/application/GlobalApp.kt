6c14eb45b0481485355c974c68702f589826c18c
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-14 20:38:52 +0700

    Khởi tạo GlobalApp và cấu hình SharedPrefCommon

---
+++ app/src/main/java/com/example/myapplication/application/GlobalApp.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.application
+
+import android.app.Application
+import com.datn.bia.a.data.storage.SharedPrefCommon
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GlobalApp: Application() {
+    companion object {
+        var jsonSearchResult: String = ""
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        SharedPrefCommon.init(this)
+    }
+}
