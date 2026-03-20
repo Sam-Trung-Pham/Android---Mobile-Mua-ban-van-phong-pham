dff8a2dde9aed3a56a4c0315c81e180c742141fd
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-20 11:55:04 +0700

    feat: thêm response model ResVoucherDTO cho mã giảm giá

---
+++ app/src/main/java/com/example/myapplication/domain/model/dto/res/ResUpdatePhoneDTO.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.domain.model.dto.res
+
+data class ResUpdatePhoneDTO (
+    val message: String = ""
+){
+}
+
+//feat: thêm response model ResUpdatePhoneDTO cho cập nhật số điện thoại
