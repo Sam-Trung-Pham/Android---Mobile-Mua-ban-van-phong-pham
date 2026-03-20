62a3508bbeb082fa826f27dea69ef84413f9df98
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-20 11:00:58 +0700

    feat: thêm entity CategoryEntity cho danh mục

---
+++ app/src/main/java/com/example/myapplication/domain/model/entity/CategoryEntity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.domain.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "CategoryEntity")
+data class CategoryEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: String,
+    val name: String,
+    val isActive: Boolean,
+    val v: Int
+) {
+}
+//feat: thêm entity CategoryEntity cho danh mục
