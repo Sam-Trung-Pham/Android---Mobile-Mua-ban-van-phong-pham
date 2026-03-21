572a0ac9d994999066e41fa43e219d69e2d4823f
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-21 15:17:31 +0700

    feat: thêm entity CommentEntity cho bình luận

---
+++ app/src/main/java/com/example/myapplication/domain/model/entity/CommentEntity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.domain.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Comment_Entity")
+data class CommentEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val orderId: String = "",
+    val userId: String = "",
+    val star: Int = 0,
+) {
+}
+//feat: thêm entity CommentEntity cho bình luận
