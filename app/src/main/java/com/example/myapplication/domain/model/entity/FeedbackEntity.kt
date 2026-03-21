add80fbff2e7a09c10ad5fad4808218b94355e8b
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-21 21:30:53 +0700

    feat: thêm entity FeedbackEntity cho phản hồi sản phẩm

---
+++ app/src/main/java/com/example/myapplication/domain/model/entity/FeedbackEntity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.domain.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "FeedBackEntity")
+data class FeedbackEntity(
+    @PrimaryKey(autoGenerate = false)
+    val _id: String,
+    val content: String,
+    val rating: Int,
+    val __v: Int,
+    val createdAt: String,
+    val updatedAt: String,
+
+    val userId: String,
+    val email: String,
+
+    val idProduct: String,
+    val productName: String,
+    val productPrice: Int,
+) {
+}
+//`feat: thêm entity FeedbackEntity cho phản hồi sản phẩm`
