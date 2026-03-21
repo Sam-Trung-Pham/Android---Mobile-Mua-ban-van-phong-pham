ff12eab0ab03aa81246555635b1580ababf5a4cd
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-21 20:16:41 +0700

    feat: thêm entity FavoriteEntity cho sản phẩm yêu thích

---
+++ app/src/main/java/com/example/myapplication/domain/model/entity/FavoriteEntity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.domain.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Favorite_Entity")
+data class FavoriteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val productId: String,
+) {
+}
+//feat: thêm entity FavoriteEntity cho sản phẩm yêu thích
