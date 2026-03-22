37a18de6d4057fcc9aef5414b506d42a2858e096
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-22 20:32:48 +0700

    feat: bổ sung xử lý tạo bình luận và bắt lỗi cho CreateCommentUseCase

--- app/src/main/java/com/example/myapplication/domain/usecase/comment/CreateCommentUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/comment/CreateCommentUseCase.kt
@@ -16,6 +16,21 @@ class CreateCommentUseCase @Inject constructor(
     operator fun invoke(req: ReqCommentDTO) = flow {
         emit(UiState.Loading)
         //`feat: thêm CreateCommentUseCase xử lý tạo bình luận sản phẩm`
+        try {
+            when (val response = commentRepository.createComment(req)) {
+                is ResultWrapper.Success -> emit(UiState.Success(response.value))
 
+                is ResultWrapper.GenericError -> emit(UiState.Error(response.message?.ifEmpty {
+                    context.getString(R.string.msg_wrong)
+                } ?: "Unknow Error"))
+
+                is ResultWrapper.NetworkError -> emit(UiState.Error("Network Error"))
+            }
+        } catch (e: HttpException) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        } catch (e: Exception) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        }
+        //`feat: bổ sung xử lý tạo bình luận và bắt lỗi cho CreateCommentUseCase`
     }
 }
