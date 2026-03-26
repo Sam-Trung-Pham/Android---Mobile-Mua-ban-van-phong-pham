3480a5040cd2a342f320a358db36d2d3592f29a6
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-26 19:29:01 +0700

    feat: bổ sung xử lý response và exception cho UpdatePhoneNumberUseCase

--- app/src/main/java/com/example/myapplication/domain/usecase/auth/UpdatePhoneNumberUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/auth/UpdatePhoneNumberUseCase.kt
@@ -22,6 +22,21 @@ class UpdatePhoneNumberUseCase @Inject constructor(
     ) = flow {
         emit(UiState.Loading)
         //`feat: thêm UpdatePhoneNumberUseCase xử lý cập nhật số điện thoại`
-        
+        try {
+            when (val response = authRepository.updatePhoneNumber(id, req)) {
+                is ResultWrapper.Success -> emit(UiState.Success(response.value))
+
+                is ResultWrapper.GenericError -> emit(UiState.Error(response.message?.ifEmpty {
+                    context.getString(R.string.msg_wrong)
+                } ?: "Unknow Error"))
+
+                is ResultWrapper.NetworkError -> emit(UiState.Error("Network Error"))
+            }
+        } catch (e: HttpException) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        } catch (e: Exception) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        }
+        //`feat: bổ sung xử lý response và exception cho UpdatePhoneNumberUseCase`
     }
 }
