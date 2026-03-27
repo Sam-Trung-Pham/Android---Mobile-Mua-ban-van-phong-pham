41ce418c74f0790d796b4b382133d537340b5109
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-27 13:20:58 +0700

    feat: thêm UpdateAddressUseCase xử lý cập nhật địa chỉ

---
+++ app/src/main/java/com/example/myapplication/domain/usecase/auth/UpdateAddressUseCase.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.domain.usecase.auth
+
+import android.content.Context
+import com.example.myapplication.R
+import com.example.myapplication.common.UiState
+import com.example.myapplication.data.network.factory.ResultWrapper
+import com.example.myapplication.domain.model.dto.req.ReqUpdateAddressDTO
+import com.example.myapplication.domain.repository.AuthRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class UpdateAddressUseCase @Inject constructor(
+    @ApplicationContext
+    private val context: Context,
+    private val authRepository: AuthRepository
+) {
+    operator fun invoke(
+        id: String,
+        req: ReqUpdateAddressDTO
+    ) = flow {
+        emit(UiState.Loading)
+        //`feat: thêm UpdateAddressUseCase xử lý cập nhật địa chỉ`
+
+    }
+}
