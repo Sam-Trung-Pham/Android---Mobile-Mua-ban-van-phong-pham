314656ef8c6b4401b5f9f65aa9c643eb5a6e5e11
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-27 13:35:12 +0700

    feat: bổ sung xử lý response và exception cho UpdateAddressUseCase

--- app/src/main/java/com/example/myapplication/domain/usecase/auth/UpdateAddressUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/auth/UpdateAddressUseCase.kt
@@ -22,6 +22,21 @@ class UpdateAddressUseCase @Inject constructor(
     ) = flow {
         emit(UiState.Loading)
         //`feat: thêm UpdateAddressUseCase xử lý cập nhật địa chỉ`
+        try {
+            when (val response = authRepository.updateAddress(id, req)) {
+                is ResultWrapper.Success -> emit(UiState.Success(response.value))
 
+                is ResultWrapper.GenericError -> emit(UiState.Error(response.message?.ifEmpty {
+                    context.getString(R.string.msg_wrong)
+                } ?: "Unknow Error"))
+
+                is ResultWrapper.NetworkError -> emit(UiState.Error("Network Error"))
+            }
+        } catch (e: HttpException) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        } catch (e: Exception) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        }
+        //`feat: bổ sung xử lý response và exception cho UpdateAddressUseCase`
     }
 }
