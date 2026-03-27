a040d83f8a29b9b7117ecd128d23328432632f7b
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-27 12:19:33 +0700

    feat: thêm ResetPassUseCase xử lý đặt lại mật khẩu

---
+++ app/src/main/java/com/example/myapplication/domain/usecase/auth/ResetPassUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.domain.usecase.auth
+
+import android.content.Context
+import com.example.myapplication.R
+import com.example.myapplication.common.UiState
+import com.example.myapplication.data.network.factory.ResultWrapper
+import com.example.myapplication.domain.model.dto.req.ReqResetPass
+import com.example.myapplication.domain.repository.AuthRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class ResetPassUseCase @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val authRepository: AuthRepository
+) {
+    operator fun invoke(req: ReqResetPass, token: String) = flow {
+        emit(UiState.Loading)
+        //`feat: thêm ResetPassUseCase xử lý đặt lại mật khẩu`
+
+    }
+}
