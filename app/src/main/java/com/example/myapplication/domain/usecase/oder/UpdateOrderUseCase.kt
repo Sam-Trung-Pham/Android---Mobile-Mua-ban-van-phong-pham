f0d2e2919ced4c72b7db21335e40f32764198a71
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-27 14:05:42 +0700

    feat: bổ sung xử lý cập nhật đơn hàng và bắt lỗi cho UpdateOrderUseCase

--- app/src/main/java/com/example/myapplication/domain/usecase/oder/UpdateOrderUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/oder/UpdateOrderUseCase.kt
@@ -22,6 +22,27 @@ class UpdateOrderUseCase @Inject constructor(
     ) = flow {
         emit(UiState.Loading)
         //`feat: thêm UpdateOrderUseCase xử lý cập nhật đơn hàng`
+        try {
+            when (val response = orderRepository.updateOrder(
+                idOrder, reqUpdateOrder
+            )) {
+                is ResultWrapper.Success -> emit(UiState.Success(response.value))
 
+                is ResultWrapper.GenericError -> {
+                    Log.d("debug", "ResultWrapper.GenericError: " + response.message ?: "")
+
+                    emit(UiState.Error(response.message?.ifEmpty {
+                        context.getString(R.string.msg_wrong)
+                    } ?: "Unknow Error"))
+                }
+
+                is ResultWrapper.NetworkError -> emit(UiState.Error("Network Error"))
+            }
+        } catch (e: HttpException) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        } catch (e: Exception) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        }
+        //`feat: bổ sung xử lý cập nhật đơn hàng và bắt lỗi cho UpdateOrderUseCase`
     }
 }
