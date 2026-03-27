2b325ac9095ea913c82f2343e044df918108dbe0
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-03-27 13:05:37 +0700

    feat: bổ sung xử lý response và exception cho SignUpUserUseCase

--- app/src/main/java/com/example/myapplication/domain/usecase/auth/SignUpUserUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/auth/SignUpUserUseCase.kt
@@ -19,6 +19,21 @@ class SignUpUserUseCase @Inject constructor(
     operator fun invoke(req: ReqSignUpUserDTO) = flow {
         emit(UiState.Loading)
         //`feat: thêm SignUpUserUseCase xử lý đăng ký người dùng`
+        try {
+            when (val response = authRepository.signUpUser(req)) {
+                is ResultWrapper.Success -> emit(UiState.Success(response.value))
 
+                is ResultWrapper.GenericError -> emit(UiState.Error(response.message?.ifEmpty {
+                    context.getString(R.string.msg_wrong)
+                } ?: "Unknow Error"))
+
+                is ResultWrapper.NetworkError -> emit(UiState.Error("Network Error"))
+            }
+        } catch (e: HttpException) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        } catch (e: Exception) {
+            emit(UiState.Error(e.message ?: "Unknow Error"))
+        }
+        //`feat: bổ sung xử lý response và exception cho SignUpUserUseCase`
     }
 }
