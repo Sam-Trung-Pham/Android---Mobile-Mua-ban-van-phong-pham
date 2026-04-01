e19e2d3c7d29593dfec2b89a870fb0eda8a811cc
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-04-01 22:17:43 +0700

    feat: thêm FavoriteProductsActivity hiển thị danh sách sản phẩm yêu thích

---
+++ app/src/main/java/com/example/myapplication/present/activity/favorite/FavoriteProductsActivity.kt
@@ -0,0 +1,62 @@
+package com.example.myapplication.present.activity.favorite
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplication.R
+import com.example.myapplication.common.AppConst
+import com.example.myapplication.common.UiState
+import com.example.myapplication.common.base.BaseActivity
+import com.example.myapplication.common.base.ext.click
+import com.example.myapplication.common.base.ext.goneView
+import com.example.myapplication.common.base.ext.showToastOnce
+import com.example.myapplication.common.base.ext.visibleView
+import com.example.myapplication.databinding.ActivityFavoriteProductsBinding
+import com.example.myapplication.present.activity.prod.ProductActivity
+import com.example.myapplication.present.fragment.home.adapter.ProductAdapter
+import com.google.gson.Gson
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class FavoriteProductsActivity : BaseActivity<ActivityFavoriteProductsBinding>() {
+    private val viewModel: FavoriteViewModel by viewModels()
+
+    private var productAdapter: ProductAdapter? = null
+
+    override fun getLayoutActivity(): Int = R.layout.activity_favorite_products
+    override fun initViews() {
+        super.initViews()
+
+        binding.rcvFavorite.apply {
+            productAdapter = ProductAdapter(
+                contextParams = thiscom.datn.bia.a.present.activity.favorite.FavoriteProductsActivity,
+                onProductClick = { index, product ->
+                    Gson().toJson(product)?.let { json ->
+                        startActivity(
+                            Intent(
+                                thiscom.datn.bia.a.present.activity.favorite.FavoriteProductsActivity,
+                                ProductActivity::class.java
+                            ).apply {
+                                putExtra(AppConst.KEY_PRODUCT_DETAIL, json)
+                            })
+                    } ?: run {
+                        showToastOnce(getString(R.string.msg_wrong))
+                    }
+                }
+            )
+
+            adapter = productAdapter
+        }
+    }
+    override fun onClickViews() {
+        super.onClickViews()
+
+        binding.icChat.click {
+            finish()
+        }
+    }
+    //`feat: thêm FavoriteProductsActivity hiển thị danh sách sản phẩm yêu thích`
+
+}
