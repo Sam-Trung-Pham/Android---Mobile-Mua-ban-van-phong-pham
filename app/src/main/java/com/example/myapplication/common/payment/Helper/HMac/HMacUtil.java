048938c002dc148318215e029ccb338f318fe607
Hoangchapter <139974027+Hoangchapter@users.noreply.github.com>
2026-04-02 23:35:16 +0700

    Thêm HMacUtil hỗ trợ mã hóa HMAC và encode Base64, Hex

--- app/src/main/java/com/example/myapplication/common/payment/Helper/HMac/HMacUtil.java
+++ app/src/main/java/com/example/myapplication/common/payment/Helper/HMac/HMacUtil.java
@@ -45,5 +45,48 @@ public class HMacUtil {
 
         return macGenerator.doFinal(dataByte);
     }
+    /**
+     * Calculating a message authentication code (MAC) involving a cryptographic
+     * hash function in combination with a secret cryptographic key.
+     *
+     * The result will be represented base64-encoded string.
+     *
+     * @param algorithm A cryptographic hash function (such as MD5 or SHA-1)
+     *
+     * @param key A secret cryptographic key
+     *
+     * @param data The message to be authenticated
+     *
+     * @return Base64-encoded HMAC String
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String HMacBase64Encode(final String algorithm, final String key, final String data) {
+        byte[] hmacEncodeBytes = HMacEncode(algorithm, key, data);
+        if (hmacEncodeBytes == null) {
+            return null;
+        }
+        return Base64.getEncoder().encodeToString(hmacEncodeBytes);
+    }
 
+    /**
+     * Calculating a message authentication code (MAC) involving a cryptographic
+     * hash function in combination with a secret cryptographic key.
+     *
+     * The result will be represented hex string.
+     *
+     * @param algorithm A cryptographic hash function (such as MD5 or SHA-1)
+     *
+     * @param key A secret cryptographic key
+     *
+     * @param data The message to be authenticated
+     *
+     * @return Hex HMAC String
+     */
+    public static String HMacHexStringEncode(final String algorithm, final String key, final String data) {
+        byte[] hmacEncodeBytes = HMacEncode(algorithm, key, data);
+        if (hmacEncodeBytes == null) {
+            return null;
+        }
+        return HexStringUtil.byteArrayToHexString(hmacEncodeBytes);
+    }
 }
