668265d2e9425e3d95431f082fe2348866bfe89a
samptph48164-beep <samptph48164@fpt.edu.vn>
2026-04-04 12:20:56 +0700

    feat: thêm OrderStateAdapter hiển thị danh sách trạng thái đơn hàng

---
+++ app/src/main/java/com/example/myapplication/present/activity/order/history/OrderStateAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication.present.activity.order.history
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.databinding.ViewDataBinding
+import com.example.myapplication.R
+import com.example.myapplication.common.base.BaseRecyclerViewAdapter
+import com.example.myapplication.common.base.ext.click
+import com.example.myapplication.common.base.ext.invisibleView
+import com.example.myapplication.common.base.ext.setTextColorById
+import com.example.myapplication.common.base.ext.visibleView
+import com.example.myapplication.databinding.ItemStatusBinding
+import com.example.myapplication.domain.model.domain.OrderState
+
+class OrderStateAdapter(
+    private val contextParams: Context,
+    private val onStatusClick: (index: Int, item: OrderState) -> Unit
+) : BaseRecyclerViewAdapter<OrderState>() {
+
+    var indexSelect: Int = 0
+        set(value) {
+            val indexSelected = field
+            field = value
+            notifyItemChanged(indexSelected)
+            notifyItemChanged(value)
+        }
+
+    override fun getItemLayout(): Int = R.layout.item_status
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun submitData(newData: List<OrderState>) {
+        list.apply {
+            clear()
+            addAll(newData)
+            notifyDataSetChanged()
+        }
+    }
+    //``
+
+
+}
